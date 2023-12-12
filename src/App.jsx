3667882c4d84ef263296959033ffc36ad3f3cb15
V Avinash <loginavi1@gmail.com>
2023-12-12 13:12:24 +0530

    desktop layout initialized

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+import './App.css'
+import DmainPg from './Page/Desktop-view/DmainPg'
+
+function App() {
+    
+ return(<>
+     <DmainPg/>
+ </>)
+}
+
+export default App
---
+++ src/Page/Desktop-view/DmainPg.jsx
@@ -0,0 +1,85 @@
+import React from 'react'
+import styles from './Dpg.module.css'
+
+const DmainPg = () => {
+  return (
+    <>
+    <div className={styles.container} >
+          <div className={styles.left} >
+                <div className={styles.leftHead}>
+                    <h1>Pockect Notes</h1>
+                </div>
+                <div className={styles.leftBody}>
+                  
+                    <div style={{display:"flex" ,justifyContent:"start",alignItems:"center",gap:"20px",backgroundColor:"orange",padding:"20px"}} ><svg width="69" height="69" viewBox="0 0 69 69" fill="none" xmlns="http://www.w3.org/2000/svg">
+<circle cx="34.4503" cy="34.4503" r="34.4503" fill="#B38BFA"/>
+</svg><h3>Note1 </h3>
+</div>
+                    <div style={{display:"flex" ,justifyContent:"start",alignItems:"center",gap:"20px",backgroundColor:"orange",padding:"20px"}} ><svg width="69" height="69" viewBox="0 0 69 69" fill="none" xmlns="http://www.w3.org/2000/svg">
+<circle cx="34.4503" cy="34.4503" r="34.4503" fill="#B38BFA"/>
+</svg><h3>Note1 </h3>
+</div>
+                    <div style={{display:"flex" ,justifyContent:"start",alignItems:"center",gap:"20px",backgroundColor:"orange",padding:"20px"}} ><svg width="69" height="69" viewBox="0 0 69 69" fill="none" xmlns="http://www.w3.org/2000/svg">
+<circle cx="34.4503" cy="34.4503" r="34.4503" fill="#B38BFA"/>
+</svg><h3>Note1 </h3>
+</div>
+                    <div style={{display:"flex" ,justifyContent:"start",alignItems:"center",gap:"20px",backgroundColor:"orange",padding:"20px"}} ><svg width="69" height="69" viewBox="0 0 69 69" fill="none" xmlns="http://www.w3.org/2000/svg">
+<circle cx="34.4503" cy="34.4503" r="34.4503" fill="#B38BFA"/>
+</svg><h3>Note1 </h3>
+</div>
+                    <div style={{display:"flex" ,justifyContent:"start",alignItems:"center",gap:"20px",backgroundColor:"orange",padding:"20px"}} ><svg width="69" height="69" viewBox="0 0 69 69" fill="none" xmlns="http://www.w3.org/2000/svg">
+<circle cx="34.4503" cy="34.4503" r="34.4503" fill="#B38BFA"/>
+</svg><h3>Note1 </h3>
+</div>
+                    <div style={{display:"flex" ,justifyContent:"start",alignItems:"center",gap:"20px",backgroundColor:"orange",padding:"20px"}} ><svg width="69" height="69" viewBox="0 0 69 69" fill="none" xmlns="http://www.w3.org/2000/svg">
+<circle cx="34.4503" cy="34.4503" r="34.4503" fill="#B38BFA"/>
+</svg><h3>Note1 </h3>
+</div>
+                    <div style={{display:"flex" ,justifyContent:"start",alignItems:"center",gap:"20px",backgroundColor:"orange",padding:"20px"}} ><svg width="69" height="69" viewBox="0 0 69 69" fill="none" xmlns="http://www.w3.org/2000/svg">
+<circle cx="34.4503" cy="34.4503" r="34.4503" fill="#B38BFA"/>
+</svg><h3>Note1 </h3>
+</div>
+                    <div style={{display:"flex" ,justifyContent:"start",alignItems:"center",gap:"20px",backgroundColor:"orange",padding:"20px"}} ><svg width="69" height="69" viewBox="0 0 69 69" fill="none" xmlns="http://www.w3.org/2000/svg">
+<circle cx="34.4503" cy="34.4503" r="34.4503" fill="#B38BFA"/>
+</svg><h3>Note1 </h3>
+</div>
+                    <div style={{display:"flex" ,justifyContent:"start",alignItems:"center",gap:"20px",backgroundColor:"orange",padding:"20px"}} ><svg width="69" height="69" viewBox="0 0 69 69" fill="none" xmlns="http://www.w3.org/2000/svg">
+<circle cx="34.4503" cy="34.4503" r="34.4503" fill="#B38BFA"/>
+</svg><h3>Note1 </h3>
+</div>
+                    <div style={{display:"flex" ,justifyContent:"start",alignItems:"center",gap:"20px",backgroundColor:"orange",padding:"20px"}} ><svg width="69" height="69" viewBox="0 0 69 69" fill="none" xmlns="http://www.w3.org/2000/svg">
+<circle cx="34.4503" cy="34.4503" r="34.4503" fill="#B38BFA"/>
+</svg><h3>Note1 </h3>
+</div>
+                    <div style={{display:"flex" ,justifyContent:"start",alignItems:"center",gap:"20px",backgroundColor:"orange",padding:"20px"}} ><svg width="69" height="69" viewBox="0 0 69 69" fill="none" xmlns="http://www.w3.org/2000/svg">
+<circle cx="34.4503" cy="34.4503" r="34.4503" fill="#B38BFA"/>
+</svg><h3>Note1 </h3>
+</div>
+                    <div style={{display:"flex" ,justifyContent:"start",alignItems:"center",gap:"20px",backgroundColor:"orange",padding:"20px"}} ><svg width="69" height="69" viewBox="0 0 69 69" fill="none" xmlns="http://www.w3.org/2000/svg">
+<circle cx="34.4503" cy="34.4503" r="34.4503" fill="#B38BFA"/>
+</svg><h3>Note1 </h3>
+</div>
+                    <div style={{display:"flex" ,justifyContent:"start",alignItems:"center",gap:"20px",backgroundColor:"orange",padding:"20px"}} ><svg width="69" height="69" viewBox="0 0 69 69" fill="none" xmlns="http://www.w3.org/2000/svg">
+<circle cx="34.4503" cy="34.4503" r="34.4503" fill="#B38BFA"/>
+</svg><h3>Note1 </h3>
+</div>
+                    <div style={{display:"flex" ,justifyContent:"start",alignItems:"center",gap:"20px",backgroundColor:"orange",padding:"20px"}} ><svg width="69" height="69" viewBox="0 0 69 69" fill="none" xmlns="http://www.w3.org/2000/svg">
+<circle cx="34.4503" cy="34.4503" r="34.4503" fill="#B38BFA"/>
+</svg><h3>Note1 </h3>
+</div>
+                    <div style={{display:"flex" ,justifyContent:"start",alignItems:"center",gap:"20px",backgroundColor:"orange",padding:"20px"}} ><svg width="69" height="69" viewBox="0 0 69 69" fill="none" xmlns="http://www.w3.org/2000/svg">
+<circle cx="34.4503" cy="34.4503" r="34.4503" fill="#B38BFA"/>
+</svg><h3>Note1 </h3>
+</div>
+                    
+                    
+                </div>
+
+          </div>
+          <div className={styles.right} ></div>
+    </div>
+    </>
+  )
+}
+
+export default DmainPg
