cbf9e0dfe1445b9fef7570c0db2c1c90b1d5c842
V Avinash <loginavi1@gmail.com>
2023-12-15 01:54:08 +0530

    Name authentication part done

--- src/Page/Main-page/DmainPg.jsx
+++ src/Page/Main-page/DmainPg.jsx
@@ -2,9 +2,43 @@ import React, { useState, useEffect, useRef } from "react";
 import styles from "./Dpg.module.css";
 import { MdLock } from "react-icons/md";
 import bgImg from "../../assets/mainPgImg.png";
-
+import LeftBody from "../../components/LeftBody";
+import PopUp from '../../components/PopUp'
+import { nanoid } from 'nanoid'
 const DmainPg = () => {
   const [popUp, setpopUp] = useState(false);
+  const [notes,setNotes]= useState([
+    {
+      'sd f': {
+        color: "#FF79F2",
+        details: []
+      }
+    },
+    {
+      'kj k j' : {
+        color: "#6691FF",
+        details: []
+      }
+    },
+    {
+      'j kh gh': {
+        color: "#B38BFA",
+        details: []
+      }
+    },
+    {
+      'klj lkj': {
+        color: "#B38BFA",
+        details: []
+      }
+    }
+  ])
+   
+  
+  
+
+
+
   let menuRef = useRef();
   useEffect(() => {
     const handleouterClick = (e) => {
@@ -24,86 +58,7 @@ const DmainPg = () => {
           <div className={styles.leftHead}>
             <h1>Pockect Notes</h1>
           </div>
-          <div className={styles.leftBody}>
-            <div className={styles.notesSec}>
-              <div className={styles.NotesDp}>
-                <h1>NM</h1>
-              </div>
-              <h3 className={styles.NotesName}>Note 1 </h3>
-            </div>
-
-            <div className={styles.notesSec}>
-              <div className={styles.NotesDp}>
-                <h1>NM</h1>
-              </div>
-              <h3 className={styles.NotesName}>Note 1 </h3>
-            </div>
-
-            <div className={styles.notesSec}>
-              <div className={styles.NotesDp}>
-                <h1>NM</h1>
-              </div>
-              <h3 className={styles.NotesName}>Note 1 </h3>
-            </div>
-
-            <div className={styles.notesSec}>
-              <div className={styles.NotesDp}>
-                <h1>NM</h1>
-              </div>
-              <h3 className={styles.NotesName}>Note 1 </h3>
-            </div>
-            <div className={styles.notesSec}>
-              <div className={styles.NotesDp}>
-                <h1>NM</h1>
-              </div>
-              <h3 className={styles.NotesName}>Note 1 </h3>
-            </div>
-            <div className={styles.notesSec}>
-              <div className={styles.NotesDp}>
-                <h1>NM</h1>
-              </div>
-              <h3 className={styles.NotesName}>Note 1 </h3>
-            </div>
-            <div className={styles.notesSec}>
-              <div className={styles.NotesDp}>
-                <h1>NM</h1>
-              </div>
-              <h3 className={styles.NotesName}>Note 1 </h3>
-            </div>
-
-            <div className={styles.notesSec}>
-              <div className={styles.NotesDp}>
-                <h1>NM</h1>
-              </div>
-              <h3 className={styles.NotesName}>Note 1 </h3>
-            </div>
-
-            <div className={styles.notesSec}>
-              <div className={styles.NotesDp}>
-                <h1>NM</h1>
-              </div>
-              <h3 className={styles.NotesName}>Note 1 </h3>
-            </div>
-
-            <div className={styles.notesSec}>
-              <div className={styles.NotesDp}>
-                <h1>NM</h1>
-              </div>
-              <h3 className={styles.NotesName}>Note 1 </h3>
-            </div>
-            <div className={styles.notesSec}>
-              <div className={styles.NotesDp}>
-                <h1>NM</h1>
-              </div>
-              <h3 className={styles.NotesName}>Note 1 </h3>
-            </div>
-            <div className={styles.notesSec}>
-              <div className={styles.NotesDp}>
-                <h1>NM</h1>
-              </div>
-              <h3 className={styles.NotesName}>Note 1 </h3>
-            </div>
-          </div>
+          <LeftBody notes={notes} />
           <div
             className={styles.addBtn}
             onClick={() => {
@@ -112,52 +67,39 @@ const DmainPg = () => {
             +
           </div>
         </div>
-        <div className={styles.right}>
-          <div className={styles.rightBody}>
-            <img src={bgImg} alt="background image of working peoples" />
-            <h1>Pocket Notes</h1>
-            <p>
-              Send and receive messages without keeping your phone online.
-              <br />
-              Use Pocket Notes on up to 4 linked devices and 1 mobile phone
-            </p>
-          </div>
-          <div className={styles.rightFooter}>
-            <MdLock size={"2rem"} />
-            <h2>end-to-end encrypted</h2>
-          </div>
-        </div>
+        <Right/>
       </div>
       {popUp ? (
-        <div className={styles.popUp} ref={menuRef}>
-          <div className={styles.createTab}>
-            <div className={styles.tabContainer} >
-              <h1>Create New group</h1>
-              <div className={styles.Gname}>
-                <h2>Group Name</h2> 
-                <input type="text" placeholder="Enter group name" />
-              </div>
-              <div className={styles.ColorSec}>
-                <h2>Choose colour</h2>
-                <div className={styles.colorOption}>
-                  <div className={styles.colorPalates} style={{backgroundColor:"#B38BFA"}} ></div>
-                  <div className={styles.colorPalates} style={{backgroundColor:"#FF79F2"}}></div>
-                  <div className={styles.colorPalates} style={{backgroundColor:"#43E6FC"}}></div>
-                  <div className={styles.colorPalates} style={{backgroundColor:"#F19576"}}></div>
-                  <div className={styles.colorPalates} style={{backgroundColor:"#0047FF"}}></div>
-                  <div className={styles.colorPalates} style={{backgroundColor:"#6691FF"}}></div>
-                </div>
-              </div>
-            </div>
-            <div className={styles.createBtn} >  <button  >Create</button></div>
-           
-          </div>
-        </div>
+        <PopUp menuRef={menuRef} notes={notes} setNotes={setNotes}  setpopUp={setpopUp} />
       ) : (
         <></>
       )}
     </>
   );
 };
+
+
+
+
+ export const Right=()=>{
+  return (
+    <div className={styles.right}>
+    <div className={styles.rightBody}>
+      <img src={bgImg} alt="background image of working peoples" />
+      <h1>Pocket Notes</h1>
+      <p>
+        Send and receive messages without keeping your phone online.
+        <br />
+        Use Pocket Notes on up to 4 linked devices and 1 mobile phone
+      </p>
+    </div>
+    <div className={styles.rightFooter}>
+      <MdLock size={"2rem"} />
+      <h2>end-to-end encrypted</h2>
+    </div>
+  </div>
+  )
+}
+
 
 export default DmainPg;
---
+++ src/components/LeftBody.jsx
@@ -0,0 +1,41 @@
+import { nanoid } from 'nanoid'
+import styles from './leftBody.module.css'
+
+
+export default function LeftBody({notes}){
+
+    function toShort (key){
+  
+       if(Object.keys(key).length==1){
+            let letter = key[0];    
+            let capWord =letter.charAt(letter.slice(0,1)).toUpperCase();  
+            return capWord;
+          }else{
+            let letter1 = key[0];    
+            let letter2 = key[1];    
+            let capWord = letter1.charAt(letter1.slice(0,1)).toUpperCase()+letter2.charAt(letter1.slice(0,1)).toUpperCase();
+            return capWord;
+        }
+        
+    }
+    function showRight(e){
+         console.log(e.target.id)
+        //  console.log(e.target.div[0].getAttribute('key'))
+    }
+
+    return(
+      <div className={styles.leftBody}>
+
+        {notes?notes.map((note)=>{ 
+            let key = Object.keys(note);
+            let shortForm = toShort(key[0].split(" "));
+            return (  <div key={nanoid()} id={Object.keys(note)}  className={styles.notesSec} onClick={(e)=>{showRight(e)}} >
+                <div style={{backgroundColor:note[key].color}} className={styles.NotesDp}>
+                  <h1>{shortForm}</h1>
+                </div>
+                <h3 className={styles.NotesName}>{key}</h3>
+              </div>)
+         }):null}                                                      
+    </div>
+    )
+  }
